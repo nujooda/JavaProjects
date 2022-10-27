6d6068fe76930f07a1efc5b6f16421c26b5fba60
Mansor Almossa <malmossa@gmail.com>
2022-10-27 05:50:33 -0700

    files completed

--- InventoryManagement/src/main/java/com/example/inventorymanagement/Models/InHouse.java
+++ InventoryManagement/src/main/java/com/example/inventorymanagement/Models/InHouse.java
@@ -1,4 +1,19 @@
 package com.example.inventorymanagement.Models;
 
-public class InHouse {
+/** Extension of part model as specified in UML diagram. */
+public class InHouse extends Part {
+    private int machineId;
+
+    public InHouse(int id, String name, double price, int stock, int min, int max, int machineId) {
+        super(id, name, price, stock, min, max);
+        this.machineId = machineId;
+    }
+
+    public int getMachineId() {
+        return machineId;
+    }
+
+    public void setMachineId(int machineId) {
+        this.machineId = machineId;
+    }
 }
